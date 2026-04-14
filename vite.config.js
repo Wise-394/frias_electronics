f0df59ef3adfd1969f5d61168f1fe1ad3f01b253
wise <114738222+Wise-394@users.noreply.github.com>
2026-04-14 21:44:59 +0800

    fix: define __dirname for ESM

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@": path.resolve(__dirname, "./src"),
+      "@styles": resolve("src/styles"),
+      "@": resolve("src"),
     },
-  },
-  test: {
-    environment: "jsdom",
-    globals: true,
   },
 });
